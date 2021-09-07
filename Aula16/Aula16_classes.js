08de69defb1c723f329cc3f0a8bb94a52a5db28f
Gabriel Lima <gpereira@blueedtech.com.br>
2021-09-06 22:17:15 -0300

    Código comentado.

--- Aula16/Aula16_classes.js
+++ Aula16/Aula16_classes.js
@@ -1,31 +1,5 @@
 const prompt = require('prompt-sync')();
 
-// Quando defino classes, uso a primeira letra maiúscula
-// Para objetos eu uso minúscula
-class Carro {
-    constructor(marca, modelo, ano, proprietario_carro){
-    this.marca = marca,
-    this.modelo = modelo,
-    this.ano = ano,
-    this.proprietario = proprietario_carro
-    }
-
-    // Declarando um método
-    // Embora não esteja com o "function", isso é uma função.
-    // As funções que estão dentro dos objetos são "métodos"
-    // Todos os objetos criados a partir dessa classe tem esse método
-    // Só é possível utiliza-los após criados os objetos
-    acelerar() {
-        console.log(`${this.modelo} faz VRRUM VRRUUUUUUUUMMM`);
-    }
-}
-
-var marca = prompt("Digite a marca do carro: ");
-let carro1 = new Carro("VW", "Fusca",1977,"Joyce");
-let carro2 = new Carro(marca, "i9", 2021, "Manuel");
-
-carro1.acelerar();
-carro2.acelerar();
 
 
 // REVISÃO FUNÇÕES
@@ -39,6 +13,79 @@ carro2.acelerar();
 // // Chamando a função, passo os argumentos que eu quero
 // let usuario = prompt("Digite seu nome: ");
 // somar(usuario, 827, 932);
+
+
+
+
+// CLASSES!!!
+
+/*
+Uma classe é um molde de objetos. Ela define como serão os objetos criados a partir dela
+É nela que são definidos os atributos (características) e métodos (ações) que os objetos terão!
+Todos os objetos instanciados (criados) a partir dessa classe, vão ter essas informações.
+A classe por si só não faz nada!! 
+Ela não possui atributos nem métodos, ela define como serão os atributos e métodos dos objetos criados a partir dela
+Então, para que eu possa "usar" essas informações, eu preciso criar um objeto antes
+E só depois disso posso manipulá-lo como eu quiser.
+*/
+
+// Quando defino classes, uso a primeira letra maiúscula
+// Para objetos eu uso minúscula
+class Carro {
+    // O método construtor é um método especial. 
+    // Ele é executado assim que crio um novo objeto, e é ele que define quais valores
+    // os atributos receberão para esse objeto.
+    // Esses valores geralmente são passados como argumentos e após isso, o construtor passa cada valor para os atributos.
+    constructor(marca, modelo, ano, proprietario_carro){
+    this.marca = marca,
+    this.modelo = modelo,
+    this.ano = ano,
+    this.proprietario = proprietario_carro // O argumento não precisa obrigatoriamente ter o mesmo nome do atributo
+    }
+
+    // Declarando um método:
+    // Embora não esteja com o "function", isso é uma função.
+    // As funções que estão dentro dos objetos são "métodos"
+    // Todos os objetos criados a partir dessa classe tem esse método
+    // Só é possível utiliza-los após criados os objetos
+    acelerar() {
+        console.log(`${this.modelo} faz VRRUM VRRUUUUUUUUMMM`);
+    }
+
+    vender() {
+        let resposta = prompt(`Você quer mesmo vender o ${this.modelo}???`);
+        if (resposta.toUpperCase() == "SIM"){ 
+            let novoprop = prompt("Digite o nome do novo proprietário: ")
+            this.proprietario = novoprop
+            console.log(`O carro foi vendido para ${this.proprietario}`)
+        } else {
+            console.log("Ok, vou manter no seu nome.")
+        }
+        
+    }
+
+}
+
+var marca = prompt("Digite a marca do carro: ");
+
+// Criando objetos:
+// Nesse momento estou criando objetos a partir da classe Carro.
+// Esses objetos obedecerão todos os critérios definidos pela classe,
+// então terão todos os atributos e métodos estabelecidos.
+// Os valores dos atributos são passados como argumentos, e o construtor (definido na classe)
+// será o responsável por pegar cada uma dessas informações e passar para um atributo.
+let carro1 = new Carro("VW", "Fusca", 1977, "Joyce");
+let carro2 = new Carro(marca, "i9", 2021, "Manuel");
+
+// Os métodos precisam de um objeto para serem chamados
+// Após instanciar o objeto, eu chamo o método de cada um, e ele será executado 
+// a partir do objeto, e não da classe!!!
+carro1.acelerar();
+carro2.acelerar();
+
+
+
+
 
 
 
